c73dccc705ec55a8832b5d6c1616b442a1ac3e63
OleksandrMyronov <cetlDriveLab@gmail.com>
2021-07-19 17:16:41 +0300

    Adding presentation

--- Assignment9_3/server.R
+++ Assignment9_3/server.R
@@ -1,33 +1,27 @@
-# This is the server logic of a Shiny web application. You can run the
+# Server logic of a Shiny web application
 library(shiny)
 library(AER)
 library(dplyr)
+library(intrval)
 library(ggplot2)
 data(HousePrices)
-hp<-mutate(HousePrices, driveway=as.numeric(factor(driveway, levels = c("no","yes"),ordered=T))-1,
-           recreation=as.numeric(factor(recreation, levels = c("no","yes"),ordered=T))-1,
-           fullbase=as.numeric(factor(fullbase, levels = c("no","yes"),ordered=T))-1,
-           gasheat=as.numeric(factor(gasheat, levels = c("no","yes"),ordered=T))-1,
-           aircon=as.numeric(factor(aircon, levels = c("no","yes"),ordered=T))-1,
-           prefer=as.numeric(factor(prefer, levels = c("no","yes"),ordered=T))-1)
+# Text variable yes/no to numeric conversion 
+fToN <- function(x) {as.numeric(factor(x, levels = c("no","yes"),ordered=T))-1}
+hp<-mutate(HousePrices, driveway=fToN(driveway), recreation=fToN(recreation),
+           fullbase=fToN(fullbase), gasheat=fToN(gasheat),
+           aircon=fToN(aircon), prefer=fToN(prefer))            
+fit_line<-lm(price~.-1-bedrooms, data=hp)      #Fitting model
 
-fit_line<-lm(price~.-1-bedrooms, data=hp)   #Fitting model
-
-#RadioButton output is 0 for True, 1 for False and 2 for Non-specified
-#output is equal inverted input for 0 or 1. When input is 2, output is 1 for max and 0 for min
-
-# Define server logic
-shinyServer(function(input, output) {
-
-#Function for constructing min and max prediction dataframes
-#Xpoint is 1 or 2 point of slidebar, Level is minimum or maximum
-predicted_Range<-function(Xpoint, Level) {
-    LevelVAL<-ifelse(Level=="min", 1, 2)
-    LevelFUN<-ifelse(Level=="min", 
+shinyServer(function(input, output) {          #Define server logic
+    #Function for constructing min and max prediction dataframes
+    #Xpoint is 1-st or 2-nd of slidebar, Level is min or max
+    predicted_Range<-function(Xpoint, Level) {
+        LevelVAL<-ifelse(Level=="min", 1, 2)     
+        LevelFUN<-ifelse(Level=="min",         #Logic for RadioButtons
                      function(x) as.numeric(x==0), 
                      function(x) as.numeric(x!=1)) 
-    data.frame(lotsize=input$binSize[Xpoint], 
-               bedrooms=1,                   #Variable excluded from linear model
+        data.frame(lotsize=input$binSize[Xpoint], 
+               bedrooms=0,                #bedrooms excluded from linear model
                bathrooms=input$binBathrooms[LevelVAL],
                stories=input$binStories[LevelVAL], 
                driveway=LevelFUN(input$radioDriveway),
@@ -37,10 +31,10 @@ predicted_Range<-function(Xpoint, Level) {
                aircon=LevelFUN(input$radioAircon),
                garage=input$binGarage[LevelVAL], 
                prefer=LevelFUN(input$radioPrefer))    
-    }    
-    
-    output$distPlot <- renderPlot({
-       hp2<-filter(hp,                       # filtering specified data
+    }   
+    #Plotting data
+    output$distPlot <- renderPlot({       
+       hp2<-filter(hp,                    #filtering specified data
                     lotsize %[]% input$binSize,
                     bathrooms %[]% input$binBathrooms,
                     stories %[]% input$binStories,
@@ -57,29 +51,25 @@ predicted_Range<-function(Xpoint, Level) {
         Ymin2<-predict(fit_line, newdata=predicted_Range(2, "min"))
         Ymax1<-predict(fit_line, newdata=predicted_Range(1, "max"))
         Ymax2<-predict(fit_line, newdata=predicted_Range(2, "max"))
-        
         lineMin<-data.frame(x=c(input$binSize[1], input$binSize[2]), 
                             y=c(Ymin1, Ymin2))
         lineMax<-data.frame(x=c(input$binSize[1], input$binSize[2]), 
                             y=c(Ymax1, Ymax2))
-        
-        hp2<-mutate(hp2, differ=price-predict(fit_line, newdata=hp2))
-        
-        # plotting data
-        p<-ggplot(hp2, aes(x=lotsize, y=price, color=differ))+
+        # Calculating difference
+        hp2<-mutate(hp2, PriceDiff=price-predict(fit_line, newdata=hp2))
+        #Creating and plotting ggplot object
+        p<-ggplot(hp2, aes(x=lotsize, y=price, color=PriceDiff))+
             scale_colour_gradient2(low = "blue", mid = "gray", high = "red")
         p<-p+geom_point(size=3, alpha=0.5)+
-            #geom_text(aes(lotsize, price, label=round(differ)), hjust=1, vjust=1)+
-            labs(x="Lot size, square feet", y="House price")+
+            labs(x="Lot size, square feet", y="House price, $")+
             theme_light()+
             geom_line(data=lineMin, aes(x=x,y=y), color="blue", lwd=1.5, alpha=0.5)+
             geom_line(data=lineMax, aes(x=x,y=y), color="red", lwd=1.5, alpha=0.5)
-        
-            if (input$checkShowPrice) {p<-p+geom_text(aes(lotsize, price, label=paste(price, "$")), hjust=0, vjust=1, color="black")}
-            if (input$checkShowDiffer) {p<-p+geom_text(aes(lotsize, price, label=paste(round(differ), "$")), hjust=0, vjust=2, color="black")}
-        p
+            if (input$checkShowPrice) {p<-p+geom_text(aes(lotsize, price, 
+                                       label=paste(price, "$")), hjust=0, vjust=1, color="black")}
+            if (input$checkShowDiffer) {p<-p+geom_text(aes(lotsize, price, 
+                                       label=paste(round(PriceDiff), "$")), hjust=0, vjust=2, color="black")}
+        p 
     })
-
-    output$Linear_Model<-renderPrint(summary(fit_line))
-    output$Descript<-renderText({summary(fit_line)})
+    output$Linear_Model<-renderPrint(summary(fit_line))  #Printing model summary 
 })
--- Assignment9_3/ui.R
+++ Assignment9_3/ui.R
@@ -1,16 +1,15 @@
 # This is the user-interface definition of a Shiny web application.
 library(shiny)
-library(intrval)
 library(shinythemes)
 
-My_RadioButton<-function(inputId, label) {                              #Custom RadioButton creation
+My_RadioButton<-function(inputId, label) {          #Custom RadioButton creation
     radioButtons(inputId, label, choiceNames=c("Yes", "No", "Non-spec"), 
                  choiceValues=c(0, 1, 2), inline=T, selected=2)}
 
 # Define UI for application
 shinyUI(fluidPage(
     theme = shinytheme("slate"), 
-    titlePanel("House Prices Data"),
+    titlePanel("House Prices App"),
     sidebarLayout(
         sidebarPanel(
             sliderInput("binSize", "Lot size in square feet",
@@ -32,17 +31,35 @@ shinyUI(fluidPage(
         mainPanel(                       # Main panel items
             tabsetPanel(type = "tabs",
                         tabPanel("Description", 
-                                 h4("This app is based on HousePrices dataset from AER package. Data represent sales 
-                                    prices of houses sold in the city of Windsor, Canada, during July, August and September, 1987."),
-                                 h3("How to use App"),
-                                 h4("Move the sliders and RadioButtons to find house of your dreams for the best price!"), 
-                                 h4("Points on scatterplot represent houses from dataset with specified parameter ranges. Use checkboxes under the plot 
-                                    to display actual house prices and difference between actual and predicted prices")
+                                 h4("This app is based on", tags$a(href="https://www.rdocumentation.org/packages/AER/versions/1.2-9/topics/
+                                    HousePrices", "HousePrices"), "dataset from AER R package. Data represent sales prices of houses sold in 
+                                    the city of Windsor, Canada, during July, August and September, 1987."),
+                                 h4("Find the house of your dreams for the best price!"),
+                                 h3("How to use App:"),
+                                 h4("1. Go to the Data Visualization Tab."),
+                                 h4("2. Select desired ranges for numeric parameters on sliders."),
+                                 h4("3. Specify desired factors on RadioButtons, Non-spec selects both cases."),
+                                 h4("4. Scatterplot displays selected data points from original dataset. Red and blue lines represent 
+                                 predictions for the most upper and the most lower of specified values and factors."),
+                                 h4("5. If there are no plotted data points, there are no such houses for sale. Try other parameters,
+                                    or you can just estimate price range by prediction lines."),
+                                 h4("6. Use checkboxes under the plot to display actual pice and (actual - predicted) price difference. 
+                                    Negative difference means, that real price is lower, than predicted."), 
+                                 h4("7. Using text labels with many data points makes plot messy. 
+                                    You can just evaluate actual-predicted price difference by color. Reds are expensive, blues are cheap."),
+                                 h4("You can find some model description on \"Linear Model Info\" tab."),
                                  ),
-                        tabPanel("Data visualization", plotOutput("distPlot"),
+                        tabPanel("Data Visualization", plotOutput("distPlot"),
                                  checkboxInput("checkShowPrice", "Show price label"),
                                  checkboxInput("checkShowDiffer", "Show difference between actual and predicted price"),
                                  ),
-                        tabPanel("Linear model", verbatimTextOutput("Linear_Model"))))
+                        tabPanel("Linear Model Info", 
+                                 h5("We used simple linear model for house price prediction algorithm. Interception and number of bedrooms 
+                                 are excluded from model, due to non-significant variable coefficient p-values (>0.05). All model coefficients 
+                                 are positive, that makes finding min and max range prediction lines very straightforward - we just need to 
+                                 find set of all min-s and all max-es from variable ranges. App algorithm produces two parallel lines, which are 
+                                 price-lotsize estimations for min and max sets of other model parameters in range of min and max lotsize from 
+                                 appropriate slidebar. When all the parameters are strictly defined, two lines collapses into single prediction line."),
+                                 verbatimTextOutput("Linear_Model"))))
     )
 ))
